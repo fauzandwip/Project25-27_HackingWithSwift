da55bae5f29876f13175f1752783c93cc3aa19b0
fauzandp <fauzan27dp@gmail.com>
2023-05-16 12:53:49 +0700

    Project25-27 | Edit Meme Photo App create UI change UI style add image picker

--- Project25-27/ViewController.swift
+++ Project25-27/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var importPhotoButton: UIButton!
+    @IBOutlet weak var topTextButton: UIButton!
+    @IBOutlet weak var bottomTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
 
+    func setupUI() {
+        importPhotoButton.layer.borderWidth = 3
+        importPhotoButton.layer.borderColor = UIColor.black.cgColor
+        importPhotoButton.layer.cornerRadius = 10
+        
+        topTextButton.layer.borderWidth = 3
+        topTextButton.layer.borderColor = UIColor.black.cgColor
+        topTextButton.layer.cornerRadius = 10
+        
+        bottomTextButton.layer.borderWidth = 3
+        bottomTextButton.layer.borderColor = UIColor.black.cgColor
+        bottomTextButton.layer.cornerRadius = 10
+    }
 
+    @IBAction func importPhotoTapped(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        
+        imageView.image = image
+    }
+    
+    @IBAction func topTextTapped(_ sender: Any) {
+    }
+    
+    @IBAction func bottomTextTapped(_ sender: UIButton) {
+    }
+    
 }
 
