f85919eeb8527870a078d82851bde56196523dc0
fauzandp <fauzan27dp@gmail.com>
2023-05-16 16:02:22 +0700

    Project25-27 | Edit Meme Photo App add code to add text to the top of image using Core Graphics using AVMakeRect to make ratio AspectFit

--- Project25-27/ViewController.swift
+++ Project25-27/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -13,6 +14,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var importPhotoButton: UIButton!
     @IBOutlet weak var topTextButton: UIButton!
     @IBOutlet weak var bottomTextButton: UIButton!
+    
+    var image = UIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,13 +51,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true)
         
         imageView.image = image
+        self.image = image
     }
     
     @IBAction func topTextTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "Type text for the top", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self] action in
+            guard let text = ac.textFields?[0].text else { return }
+            self?.addText(text: text)
+        })
+        
+        present(ac, animated: true)
     }
     
     @IBAction func bottomTextTapped(_ sender: UIButton) {
     }
     
+    func addText(text: String) {
+        let imageViewWidth = imageView.frame.size.width
+        let imageViewHeight = imageView.frame.size.height
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: imageViewWidth, height: imageViewHeight))
+        
+        let image = renderer.image { ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 25, weight: .semibold),
+                .foregroundColor: UIColor.red.cgColor,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let attributedString = NSAttributedString(string: text, attributes: attrs)
+            
+            let imageFitSize = AVMakeRect(aspectRatio: self.image.size, insideRect: self.imageView.bounds)
+            
+            self.image.draw(in: imageFitSize)
+            
+            let strX = (self.imageView.bounds.width - imageFitSize.width) / 2
+            let stry = (self.imageView.bounds.height - imageFitSize.height) / 2
+            let strWidth = imageFitSize.width - 40
+            let strHeight = imageFitSize.height - 40
+            
+            attributedString.draw(with: CGRect(x: strX + 20, y: stry + 20, width: strWidth, height: strHeight), options: .usesLineFragmentOrigin, context: nil)
+        }
+        
+        imageView.image = image
+    }
 }
 
