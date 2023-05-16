8be5e4a0f37cfa9710bf55b2da62c094b5893fa2
fauzandp <fauzan27dp@gmail.com>
2023-05-16 12:03:50 +0700

    Initial Commit

---
+++ Project25-27/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25-27
+//
+//  Created by Fauzan Dwi Prasetyo on 16/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
